51f59ac989342848c638577399e15255faae818a
chansecampbell <chansecampbell@gmail.com>
2016-05-28 16:20:04 +0100

    Set up a src directory to house my scss and js files within public

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,13 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+    src: ['src/js/**/*.js']
+  },
+  });
 
-  
+  grunt.registerTask('default', ['jshint']);
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
 };
