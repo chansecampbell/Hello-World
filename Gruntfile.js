9b6f1729ab313744c876aede41a901f380e5cfd2
chansecampbell <chansecampbell@gmail.com>
2016-05-28 16:31:47 +0100

    Added all of the grunt tasks. scss and js in the front end are now compiling into single files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,55 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-    src: ['src/js/**/*.js']
-  },
+      src: ['public/src/js/**/*.js']
+    },
+    sass: {
+      expanded: {
+        options: { outputStyle: 'expanded' },
+        files: { 'public/css/app.css': 'public/src/scss/app.scss' }
+      },
+      compressed: {
+        options: { outputStyle: 'compressed' },
+        files: { 'public/css/app.min.css': 'public/src/scss/app.scss' }
+      }
+    },
+    concat: {
+      dist: {
+        src: ['public/src/js/app.js', 'public/src/js/**/*.js'],
+        dest: 'public/js/app.js'
+      }
+    },
+    uglify: {
+      'public/js/app.min.js': 'public/js/app.js'
+    },
+    watch: {
+      configFiles: {
+        files: ['Gruntfile.js', 'package.json'],
+        options: { reload: true }
+      },
+      scss: {
+        files: ['public/src/scss/**/*.scss'],
+        tasks: ['sass'],
+        options: { livereload: true }
+      },
+      js: {
+        files: ['public/src/js/**/*.js'],
+        tasks: ['jshint', 'concat', 'uglify'],
+        options: { livereload: true }
+      },
+      index: {
+        files: ['index.html'],
+        options: { livereload: true }
+      }
+    }
   });
 
-  grunt.registerTask('default', ['jshint']);
-
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'sass', 'concat', 'uglify', 'watch']);
 
 };
