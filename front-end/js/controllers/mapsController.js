9f82f662921093df0412c473ac1ba2a1546cd0f6
chansecampbell <chansecampbell@gmail.com>
2016-05-30 16:34:35 +0100

    Playing around with layers on maps still. Managed to highlight every country from the HTML

--- front-end/js/controllers/mapsController.js
+++ front-end/js/controllers/mapsController.js
@@ -2,9 +2,8 @@ angular
   .module('logging')
   .controller('MapsController', MapsController);
 
-// MainController.$inject = ['$auth'];
 function MapsController(NgMap) {
-  
+
   this.hello = function(){
     console.log('hello!');
   };
