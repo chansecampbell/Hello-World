7f7d94f268d309cb412ee4d9454259c790ffc191
chansecampbell <chansecampbell@gmail.com>
2016-05-31 17:46:12 +0100

    I can now add a database country from a journey onto the map! It duplicates atm though.

--- front-end/js/controllers/countriesController.js
+++ front-end/js/controllers/countriesController.js
@@ -22,39 +22,63 @@ function CountriesController(Country, Journey, $state) {
 
   function getJourneys() {
     Journey.query(function(data){
-      self.journeys = data.journeys;
-      console.log(self.journeys[0].country.countryCode);
-    });
+      self.journeys = data.journeys[0].country.countryCode;
+      // console.log(self.journeys);
+      self.colour = '#33ccff';
+
+
+      self.countries = {};
+
+      self.countries[self.journeys] = self.colour;
+
+      console.log(self.countries);
+
+      self.map = 
+      $('#world-map').vectorMap({
+        map: 'world_mill_en',
+        // backgroundColor: '#f5f5f5',
+       series: {
+         regions: [{
+           values: self.countries
+         }]
+       }
+
+
+       });
+      });
+
+
   }
 
-  self.countries = {
-    IN:'#33ccff',
-    DK:'#33ccff',
-    KP:'#33ccff',
-    IR:'#33ccff',
-    IQ:'#33ccff',
-    PK:'#33ccff',
-    UZ:'#33ccff',
-    TM:'#33ccff',
-    EG:'#33ccff',
-    AZ:'#33ccff',
-    TJ:'#33ccff',
-    KG:'#33ccff',
-    SY:'#33ccff',
-    JO:'#33ccff',
-    KZ:'#33ccff',
-    LK:'#33ccff',
-    US:'#33ccff'
-  }
+  // self.countries = {
+  //   IN:'#33ccff',
+  //   DK:'#33ccff',
+  //   KP:'#33ccff',
+  //   IR:'#33ccff',
+  //   IQ:'#33ccff',
+  //   PK:'#33ccff',
+  //   UZ:'#33ccff',
+  //   TM:'#33ccff',
+  //   EG:'#33ccff',
+  //   AZ:'#33ccff',
+  //   TJ:'#33ccff',
+  //   KG:'#33ccff',
+  //   SY:'#33ccff',
+  //   JO:'#33ccff',
+  //   KZ:'#33ccff',
+  //   LK:'#33ccff',
+  //   US:'#33ccff'
+  // }
 
   self.map = 
   $('#world-map').vectorMap({
-    map: 'world_mill_en',
-   series: {
-     regions: [{
-       values: self.countries
-     }]
-   }
+    map: 'world_mill_en'
+    // backgroundColor: '#f5f5f5',
+   // series: {
+   //   regions: [{
+   //     values: self.countries
+   //   }]
+   // }
 
 
    });
