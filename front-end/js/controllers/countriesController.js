560f35c9acaf6ef694e985fc03e46bdeab51fe05
chansecampbell <chansecampbell@gmail.com>
2016-06-01 20:46:47 +0100

    Users info now shows up on the world mapgit add -Agit add -Agit add -A

--- front-end/js/controllers/countriesController.js
+++ front-end/js/controllers/countriesController.js
@@ -7,8 +7,8 @@ function CountriesController(Country, Journey, User, $state, CurrentUser) {
   var self            = this;
   self.all            = [];
   self.getCountries   = getCountries;
-  self.countries      = {};
-  self.mapOutCountries = mapOutCountries;
+  self.countries      = [];
+  // self.mapOutCountries = mapOutCountries;
   self.getUsers        = getUsers;
 
 
@@ -28,78 +28,43 @@ function CountriesController(Country, Journey, User, $state, CurrentUser) {
   function getUsers() {
     User.query(function(data){
       self.all = data.users
-      // for (var i = 0; i < 3; i++ )
-      // self.countries = {
-      //   self.all[0].countries[i].countryCode: '#3498db',
-      // }
-      // console.log(self.countries);
+      for (var i = 0; i < 3; i++ )
+        self.countries.push(self.all[0].countries[i].countryCode);
+        console.log(self.countries);
+         self.map = 
+         $('#world-map').vectorMap({
+           map: 'world_mill_en',
+           zoomButtons : false,
+           backgroundColor: '#f5f5f5',
+           selectedRegions: self.countries,
+           regionStyle: {
+            initial: {
+
+              fill: '#757575'
+            },
+            selected: {
+                fill: '#3498db'
+              }
+          }
+
+        });
     });
-  }
 
-
-  function mapOutCountries() {
-    var currentUser = CurrentUser.getUser();
-    console.log(currentUser);
-
-     self.colour = '#3498db';
-
-     self.countries["GB"] = self.colour;
-
-
-     self.map = 
-     $('#world-map').vectorMap({
-       map: 'world_mill_en',
-       zoomButtons : false,
-       backgroundColor: '#f5f5f5',
-       selectedRegions: ['GB', 'JP'],
-       regionStyle: {
-        initial: {
-
-          fill: '#757575'
-        }
-      
-
-      //   ,
-      //   hover: {
-      //     fill: "#d3d3d3"
-      //   }
-      // },
-      // series: {
-      //   regions: {[
-      //   'GB'
-      // ]},
-      //   {
-      //   values: '#3498db'
-      //   }
-      }
-
-    });
 
   }
 
 
-  // self.countries = {
-  //   IN:'#33ccff',
-  //   DK:'#33ccff',
-  //   KP:'#33ccff',
-  //   IR:'#33ccff',
-  //   IQ:'#33ccff',
-  //   PK:'#33ccff',
-  //   UZ:'#33ccff',
-  //   TM:'#33ccff',
-  //   EG:'#33ccff',
-  //   AZ:'#33ccff',
-  //   TJ:'#33ccff',
-  //   KG:'#33ccff',
-  //   SY:'#33ccff',
-  //   JO:'#33ccff',
-  //   KZ:'#33ccff',
-  //   LK:'#33ccff',
-  //   US:'#33ccff'
+  // function mapOutCountries() {
+  //   var currentUser = CurrentUser.getUser();
+  //   console.log(currentUser);
+
+
+
   // }
 
+
   self.getCountries();
-  self.mapOutCountries();
+  // self.mapOutCountries();
   self.getUsers();
 }
 
