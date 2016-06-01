457ce99af9304c95d906dd0ae3c63bf2e85feba8
chansecampbell <chansecampbell@gmail.com>
2016-06-01 21:13:34 +0100

    The map should now work for all usersgit add -Agit add -A

--- front-end/js/controllers/countriesController.js
+++ front-end/js/controllers/countriesController.js
@@ -5,66 +5,59 @@ angular
 CountriesController.$inject = ['Country', 'Journey', 'User', '$state', 'CurrentUser'];
 function CountriesController(Country, Journey, User, $state, CurrentUser) {
   var self            = this;
-  self.all            = [];
-  self.getCountries   = getCountries;
+  self.users            = [];
   self.countries      = [];
-  // self.mapOutCountries = mapOutCountries;
   self.getUsers        = getUsers;
+  // self.getCountries   = getCountries;
+  // self.mapOutCountries = mapOutCountries;
 
 
-  function getCountries(country) {
-    Country.query(function(data){
-      self.all = data.countries;
-      // for (var i = 0; i < self.all.length; i++) {
-      //   // console.log(self.all[i].countryCode);
-      // }
-      // console.log(data.countries[0].name);
-      // console.log(data.countries[0].countryCode);
-     // return self.all.map(function(country) {
-     //  return country.name;
-    });
-  }
-
-  function getUsers() {
-    User.query(function(data){
-      self.all = data.users
-      for (var i = 0; i < 3; i++ )
-        self.countries.push(self.all[0].countries[i].countryCode);
-        console.log(self.countries);
-         self.map = 
-         $('#world-map').vectorMap({
-           map: 'world_mill_en',
-           zoomButtons : false,
-           backgroundColor: '#f5f5f5',
-           selectedRegions: self.countries,
-           regionStyle: {
-            initial: {
-
-              fill: '#757575'
-            },
-            selected: {
-                fill: '#3498db'
-              }
-          }
-
-        });
-    });
-
-
-  }
-
-
-  // function mapOutCountries() {
-  //   var currentUser = CurrentUser.getUser();
-  //   console.log(currentUser);
-
-
-
+  // function getCountries(country) {
+  //   Country.query(function(data){
+  //     self.all = data.countries;
+  //   });
   // }
 
+  function getUsers() {
+    self.users       = [];
+    self.countries = [];
+    User.query(function(data){
+      self.users = data.users
+      self.currentUser = CurrentUser.getUser();
+      for (var j = 0; j < self.users.length; j++){
+        if (self.users[j].name == self.currentUser.name ) {
+          for (var i = 0; i < self.users[j].countries.length; i++ ) {
+          console.log(self.users[j].countries);
+          self.countries.push(self.users[j].countries[i].countryCode);
+        }
+          $('#world-map').vectorMap({
+                     map: 'world_mill_en',
+                     zoomButtons : false,
+                     backgroundColor: '#f5f5f5',
+                     selectedRegions: self.countries,
+                     regionStyle: {
+                      initial: {
+                        fill: '#757575'
+                      },
+                      selected: {
+                        fill: '#3498db'
+                        }
+                    }
+                  });
 
-  self.getCountries();
-  // self.mapOutCountries();
+        }
+      }
+    })
+  }
+
+      // for (var i = 0; i < self.users[0].countries.length; i++ ) {
+      //   self.countries.push(self.users[0].countries[i].countryCode);
+      //   console.log(self.countries);
+      //    self.map = 
+            //  }
+ 
+
+  // self.getCountries();
   self.getUsers();
 }
 
