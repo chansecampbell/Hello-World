a9e4103316f4d463f46dd5386ce9534da02b9da7
chansecampbell <chansecampbell@gmail.com>
2016-06-21 22:34:41 +0100

    Added a fade in and fade out to my pages.

---
+++ front-end/js/world.js
@@ -0,0 +1,15 @@
+    $(document).ready(function() {
+     $("main").css("display", "none");
+     $("img").css("display", "none");
+
+     $("a.transition").click(function(event){
+     event.preventDefault();
+     linkLocation = this.href;
+     $("main").fadeOut(600, redirectPage);
+     $(".row").fadeOut(600, redirectPage);  
+     });
+
+     function redirectPage() {
+     window.location = linkLocation;
+     }
+    });
