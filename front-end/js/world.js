4e804d9766da7f9f414490341d051e7db425691e
chansecampbell <chansecampbell@gmail.com>
2016-07-04 12:22:58 +0100

    Added the readme.

--- front-end/js/world.js
+++ front-end/js/world.js
@@ -1,15 +1,15 @@
-    $(document).ready(function() {
-     $("main").css("display", "none");
-     $("img").css("display", "none");
+    // $(document).ready(function() {
+    //  $("main").css("display", "none");
+    //  $("img").css("display", "none");
 
-     $("a.transition").click(function(event){
-     event.preventDefault();
-     linkLocation = this.href;
-     $("main").fadeOut(600, redirectPage);
-     $(".row").fadeOut(600, redirectPage);  
-     });
+    //  $("a.transition").click(function(event){
+    //  event.preventDefault();
+    //  linkLocation = this.href;
+    //  $("main").fadeOut(600, redirectPage);
+    //  $(".row").fadeOut(600, redirectPage);  
+    //  });
 
-     function redirectPage() {
-     window.location = linkLocation;
-     }
-    });
+    //  function redirectPage() {
+    //  window.location = linkLocation;
+    //  }
+    // });
